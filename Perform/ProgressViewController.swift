c7f8c6da048665c39d5af3ee42c4a1c7a4d1f20f
yzd0014 <yzd0014@auburn.edu>
2016-09-12 14:31:58 -0500

    Perform Pro
    9.12.2016

---
+++ Perform/ProgressViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProgressViewController.swift
+//  Perform
+//
+//  Created by 戴一通 on 9/12/16.
+//  Copyright © 2016 ___FULLUSERNAME__. All rights reserved.
+//
+
+import UIKit
+
+class ProgressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
